e42c73922d4e47105e9974abe8143fa88e3ac764
Htet Lynn Htun <htetlynnhtun101@gmail.com>
2024-06-01 22:31:15 +0700

    Align title to the left

--- lib/feed/view/feed_item_view.dart
+++ lib/feed/view/feed_item_view.dart
@@ -33,6 +33,7 @@ class FeedItemView extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   post.title,
