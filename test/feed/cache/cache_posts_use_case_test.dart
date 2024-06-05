5916a6c48ed177274ffa1f82de52dc5f80305f35
Htet Lynn Htun <htetlynnhtun101@gmail.com>
2024-06-05 16:03:59 +0700

    `LocalFeedLoader.save` does not request cache insertion on deletion error

--- test/feed/cache/cache_posts_use_case_test.dart
+++ test/feed/cache/cache_posts_use_case_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
@@ -28,6 +29,17 @@ void main() {
       sut.save([makePost(id: 1)]);
 
       verify(mockedStore.deleteCachedPosts()).called(1);
+    });
+
+    test('save does not request cache insertion on deletion error', () async {
+      final (mockedStore, sut) = makeSUT();
+      when(mockedStore.deleteCachedPosts())
+          .thenThrow(Exception('deletion error'));
+
+      sut.save([makePost(id: 1)]).whenComplete(() {
+        verify(mockedStore.deleteCachedPosts()).called(1);
+        verify(mockedStore.insert(any)).called(0);
+      }).ignore();
     });
   });
 }
