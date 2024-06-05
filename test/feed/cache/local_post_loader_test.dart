9ca6dd528f20a4661dece71463c9169621282c35
Htet Lynn Htun <htetlynnhtun101@gmail.com>
2024-06-05 12:09:08 +0700

    `LocalPostLoader.load` fails on retrieval error

--- test/feed/cache/local_post_loader_test.dart
+++ test/feed/cache/local_post_loader_test.dart
@@ -7,7 +7,7 @@ import 'local_post_loader_test.mocks.dart';
 
 @GenerateNiceMocks([MockSpec<PostStore>()])
 void main() {
-  group('LocalPostLoader', () {
+  group('LocalPostLoader:', () {
     test('does not message store on init', () {
       final mockedStore = MockPostStore();
 
@@ -23,6 +23,15 @@ void main() {
       sut.load();
 
       verify(mockedStore.retrieve()).called(1);
+    });
+
+    test('load fails on retrieval error', () {
+      final mockedStore = MockPostStore();
+      final sut = LocalPostLoader(postStore: mockedStore);
+      final retrievalError = Exception('Retrieval error');
+      when(mockedStore.retrieve()).thenThrow(retrievalError);
+
+      expect(() => sut.load(), throwsA(retrievalError));
     });
   });
 }
