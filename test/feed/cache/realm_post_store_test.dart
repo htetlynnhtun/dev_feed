86417c4dca89743853b3cb05a47afa4ed8975145
Htet Lynn Htun <htetlynnhtun101@gmail.com>
2024-06-11 16:41:06 +0700

    `RealmPostStore.retrieve()` has no side effects on empty cache

--- test/feed/cache/realm_post_store_test.dart
+++ test/feed/cache/realm_post_store_test.dart
@@ -17,6 +17,21 @@ void main() {
 
         expect(posts.length, 0);
       });
+
+      test('has no side effects on empty cache', () async {
+        final realm = Realm(Configuration.inMemory([
+          RealmPost.schema,
+          RealmUser.schema,
+        ]));
+        addTearDown(() => realm.close());
+        final sut = RealmPostStore(realm: realm);
+
+        final firstPosts = await sut.retrieve();
+        final lastPosts = await sut.retrieve();
+
+        expect(firstPosts.length, 0);
+        expect(lastPosts.length, 0);
+      });
     });
   });
 }
