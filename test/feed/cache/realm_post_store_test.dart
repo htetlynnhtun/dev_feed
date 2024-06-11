95ea7edfda4ae0d7a837dd459f5497c4694110dd
Htet Lynn Htun <htetlynnhtun101@gmail.com>
2024-06-11 16:38:20 +0700

    `RealmPostStore.retrieve()` delivers empty posts on empty cache

---
+++ test/feed/cache/realm_post_store_test.dart
@@ -0,0 +1,22 @@
+import 'package:dev_feed/feed/cache/cache.dart';
+import 'package:realm/realm.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('RealmPostStore', () {
+    group('.retrieve()', () {
+      test('delivers empty posts on empty cache', () async {
+        final realm = Realm(Configuration.inMemory([
+          RealmPost.schema,
+          RealmUser.schema,
+        ]));
+        addTearDown(() => realm.close());
+        final sut = RealmPostStore(realm: realm);
+
+        final posts = await sut.retrieve();
+
+        expect(posts.length, 0);
+      });
+    });
+  });
+}
