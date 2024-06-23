009650b1698a6617b37f96a09803a3934cdf88ff
Htet Lynn Htun <htetlynnhtun101@gmail.com>
2024-06-23 17:42:21 +0700

    BookmarkItemViewModel `.bookmark()` requests creation of a bookmark for the post

--- test/bookmark/bookmark_item_view_model_test.dart
+++ test/bookmark/bookmark_item_view_model_test.dart
@@ -1,22 +1,74 @@
+import 'package:dev_feed/bookmark/model/bookmark.dart';
+import 'package:dev_feed/bookmark/model/bookmark_creator.dart';
+import 'package:dev_feed/posts_feed/model/post.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../helpers.dart';
 
 part 'bookmark_item_view_model_test.freezed.dart';
 
 void main() {
   test('BookmarkItemVeiwModel has correct initial state of pending', () {
-    final sut = BookmarkItemVeiwModel();
+    final sut = BookmarkItemVeiwModel(
+      post: makePost(id: 7),
+      bookmarkCreator: BookmarkSpy(),
+    );
 
     expect(sut.value, const BookmarkItemViewState.pending());
   });
+
+  test(
+    'BookmarkItemViewModel .bookmark() requests '
+    'creation of a bookmark for the post',
+    () {
+      final post = makePost(id: 7);
+      final bookmarkSpy = BookmarkSpy();
+      final sut = BookmarkItemVeiwModel(
+        post: post,
+        bookmarkCreator: bookmarkSpy,
+      );
+
+      sut.bookmark();
+
+      expect(bookmarkSpy.messages, [
+        _ReceivedMessages.createWith(post),
+      ]);
+    },
+  );
+}
+
+@freezed
+class _ReceivedMessages with _$ReceivedMessages {
+  const factory _ReceivedMessages.createWith(Post post) = CreateWith;
+}
+
+class BookmarkSpy implements BookmarkCreator {
+  final messages = <_ReceivedMessages>[];
+
+  @override
+  Future<void> createWith(Post post) async {
+    messages.add(_ReceivedMessages.createWith(post));
+  }
 }
 
 class BookmarkItemVeiwModel extends ValueNotifier<BookmarkItemViewState> {
-  BookmarkItemVeiwModel() : super(const BookmarkItemViewState.pending());
+  final BookmarkCreator bookmarkCreator;
+  final Post post;
+
+  BookmarkItemVeiwModel({
+    required this.post,
+    required this.bookmarkCreator,
+  }) : super(const BookmarkItemViewState.pending());
+
+  void bookmark() {
+    bookmarkCreator.createWith(post);
+  }
 }
 
 @freezed
 class BookmarkItemViewState with _$BookmarkItemViewState {
   const factory BookmarkItemViewState.pending() = Pending;
+  const factory BookmarkItemViewState.bookmarked() = Bookmarked;
 }
--- test/bookmark/bookmark_item_view_model_test.freezed.dart
+++ test/bookmark/bookmark_item_view_model_test.freezed.dart
@@ -15,36 +15,268 @@ final _privateConstructorUsedError = UnsupportedError(
     'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
 
 /// @nodoc
+mixin _$ReceivedMessages {
+  Post get post => throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function(Post post) createWith,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function(Post post)? createWith,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function(Post post)? createWith,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(CreateWith value) createWith,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(CreateWith value)? createWith,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(CreateWith value)? createWith,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  _$ReceivedMessagesCopyWith<_ReceivedMessages> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class _$ReceivedMessagesCopyWith<$Res> {
+  factory _$ReceivedMessagesCopyWith(
+          _ReceivedMessages value, $Res Function(_ReceivedMessages) then) =
+      __$ReceivedMessagesCopyWithImpl<$Res, _ReceivedMessages>;
+  @useResult
+  $Res call({Post post});
+
+  $PostCopyWith<$Res> get post;
+}
+
+/// @nodoc
+class __$ReceivedMessagesCopyWithImpl<$Res, $Val extends _ReceivedMessages>
+    implements _$ReceivedMessagesCopyWith<$Res> {
+  __$ReceivedMessagesCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? post = null,
+  }) {
+    return _then(_value.copyWith(
+      post: null == post
+          ? _value.post
+          : post // ignore: cast_nullable_to_non_nullable
+              as Post,
+    ) as $Val);
+  }
+
+  @override
+  @pragma('vm:prefer-inline')
+  $PostCopyWith<$Res> get post {
+    return $PostCopyWith<$Res>(_value.post, (value) {
+      return _then(_value.copyWith(post: value) as $Val);
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$$CreateWithImplCopyWith<$Res>
+    implements _$ReceivedMessagesCopyWith<$Res> {
+  factory _$$CreateWithImplCopyWith(
+          _$CreateWithImpl value, $Res Function(_$CreateWithImpl) then) =
+      __$$CreateWithImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({Post post});
+
+  @override
+  $PostCopyWith<$Res> get post;
+}
+
+/// @nodoc
+class __$$CreateWithImplCopyWithImpl<$Res>
+    extends __$ReceivedMessagesCopyWithImpl<$Res, _$CreateWithImpl>
+    implements _$$CreateWithImplCopyWith<$Res> {
+  __$$CreateWithImplCopyWithImpl(
+      _$CreateWithImpl _value, $Res Function(_$CreateWithImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? post = null,
+  }) {
+    return _then(_$CreateWithImpl(
+      null == post
+          ? _value.post
+          : post // ignore: cast_nullable_to_non_nullable
+              as Post,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$CreateWithImpl with DiagnosticableTreeMixin implements CreateWith {
+  const _$CreateWithImpl(this.post);
+
+  @override
+  final Post post;
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return '_ReceivedMessages.createWith(post: $post)';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('type', '_ReceivedMessages.createWith'))
+      ..add(DiagnosticsProperty('post', post));
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$CreateWithImpl &&
+            (identical(other.post, post) || other.post == post));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, post);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$CreateWithImplCopyWith<_$CreateWithImpl> get copyWith =>
+      __$$CreateWithImplCopyWithImpl<_$CreateWithImpl>(this, _$identity);
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function(Post post) createWith,
+  }) {
+    return createWith(post);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function(Post post)? createWith,
+  }) {
+    return createWith?.call(post);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function(Post post)? createWith,
+    required TResult orElse(),
+  }) {
+    if (createWith != null) {
+      return createWith(post);
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(CreateWith value) createWith,
+  }) {
+    return createWith(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(CreateWith value)? createWith,
+  }) {
+    return createWith?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(CreateWith value)? createWith,
+    required TResult orElse(),
+  }) {
+    if (createWith != null) {
+      return createWith(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class CreateWith implements _ReceivedMessages {
+  const factory CreateWith(final Post post) = _$CreateWithImpl;
+
+  @override
+  Post get post;
+  @override
+  @JsonKey(ignore: true)
+  _$$CreateWithImplCopyWith<_$CreateWithImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
 mixin _$BookmarkItemViewState {
   @optionalTypeArgs
   TResult when<TResult extends Object?>({
     required TResult Function() pending,
+    required TResult Function() bookmarked,
   }) =>
       throw _privateConstructorUsedError;
   @optionalTypeArgs
   TResult? whenOrNull<TResult extends Object?>({
     TResult? Function()? pending,
+    TResult? Function()? bookmarked,
   }) =>
       throw _privateConstructorUsedError;
   @optionalTypeArgs
   TResult maybeWhen<TResult extends Object?>({
     TResult Function()? pending,
+    TResult Function()? bookmarked,
     required TResult orElse(),
   }) =>
       throw _privateConstructorUsedError;
   @optionalTypeArgs
   TResult map<TResult extends Object?>({
     required TResult Function(Pending value) pending,
+    required TResult Function(Bookmarked value) bookmarked,
   }) =>
       throw _privateConstructorUsedError;
   @optionalTypeArgs
   TResult? mapOrNull<TResult extends Object?>({
     TResult? Function(Pending value)? pending,
+    TResult? Function(Bookmarked value)? bookmarked,
   }) =>
       throw _privateConstructorUsedError;
   @optionalTypeArgs
   TResult maybeMap<TResult extends Object?>({
     TResult Function(Pending value)? pending,
+    TResult Function(Bookmarked value)? bookmarked,
     required TResult orElse(),
   }) =>
       throw _privateConstructorUsedError;
@@ -115,6 +347,7 @@ class _$PendingImpl with DiagnosticableTreeMixin implements Pending {
   @optionalTypeArgs
   TResult when<TResult extends Object?>({
     required TResult Function() pending,
+    required TResult Function() bookmarked,
   }) {
     return pending();
   }
@@ -123,6 +356,7 @@ class _$PendingImpl with DiagnosticableTreeMixin implements Pending {
   @optionalTypeArgs
   TResult? whenOrNull<TResult extends Object?>({
     TResult? Function()? pending,
+    TResult? Function()? bookmarked,
   }) {
     return pending?.call();
   }
@@ -131,6 +365,7 @@ class _$PendingImpl with DiagnosticableTreeMixin implements Pending {
   @optionalTypeArgs
   TResult maybeWhen<TResult extends Object?>({
     TResult Function()? pending,
+    TResult Function()? bookmarked,
     required TResult orElse(),
   }) {
     if (pending != null) {
@@ -143,6 +378,7 @@ class _$PendingImpl with DiagnosticableTreeMixin implements Pending {
   @optionalTypeArgs
   TResult map<TResult extends Object?>({
     required TResult Function(Pending value) pending,
+    required TResult Function(Bookmarked value) bookmarked,
   }) {
     return pending(this);
   }
@@ -151,6 +387,7 @@ class _$PendingImpl with DiagnosticableTreeMixin implements Pending {
   @optionalTypeArgs
   TResult? mapOrNull<TResult extends Object?>({
     TResult? Function(Pending value)? pending,
+    TResult? Function(Bookmarked value)? bookmarked,
   }) {
     return pending?.call(this);
   }
@@ -159,6 +396,7 @@ class _$PendingImpl with DiagnosticableTreeMixin implements Pending {
   @optionalTypeArgs
   TResult maybeMap<TResult extends Object?>({
     TResult Function(Pending value)? pending,
+    TResult Function(Bookmarked value)? bookmarked,
     required TResult orElse(),
   }) {
     if (pending != null) {
@@ -170,4 +408,113 @@ class _$PendingImpl with DiagnosticableTreeMixin implements Pending {
 
 abstract class Pending implements BookmarkItemViewState {
   const factory Pending() = _$PendingImpl;
+}
+
+/// @nodoc
+abstract class _$$BookmarkedImplCopyWith<$Res> {
+  factory _$$BookmarkedImplCopyWith(
+          _$BookmarkedImpl value, $Res Function(_$BookmarkedImpl) then) =
+      __$$BookmarkedImplCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$BookmarkedImplCopyWithImpl<$Res>
+    extends _$BookmarkItemViewStateCopyWithImpl<$Res, _$BookmarkedImpl>
+    implements _$$BookmarkedImplCopyWith<$Res> {
+  __$$BookmarkedImplCopyWithImpl(
+      _$BookmarkedImpl _value, $Res Function(_$BookmarkedImpl) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$BookmarkedImpl with DiagnosticableTreeMixin implements Bookmarked {
+  const _$BookmarkedImpl();
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'BookmarkItemViewState.bookmarked()';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+        .add(DiagnosticsProperty('type', 'BookmarkItemViewState.bookmarked'));
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$BookmarkedImpl);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() pending,
+    required TResult Function() bookmarked,
+  }) {
+    return bookmarked();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? pending,
+    TResult? Function()? bookmarked,
+  }) {
+    return bookmarked?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? pending,
+    TResult Function()? bookmarked,
+    required TResult orElse(),
+  }) {
+    if (bookmarked != null) {
+      return bookmarked();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(Pending value) pending,
+    required TResult Function(Bookmarked value) bookmarked,
+  }) {
+    return bookmarked(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(Pending value)? pending,
+    TResult? Function(Bookmarked value)? bookmarked,
+  }) {
+    return bookmarked?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(Pending value)? pending,
+    TResult Function(Bookmarked value)? bookmarked,
+    required TResult orElse(),
+  }) {
+    if (bookmarked != null) {
+      return bookmarked(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class Bookmarked implements BookmarkItemViewState {
+  const factory Bookmarked() = _$BookmarkedImpl;
 }
