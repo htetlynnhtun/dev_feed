8a0bc466dc659bd46e0c8cb2a9f122b163333efc
Htet Lynn Htun <htetlynnhtun101@gmail.com>
2024-07-12 15:55:20 +0700

    Remove unused `deleteFor` method

--- apps/dev_feed/lib/bookmark/model/bookmark_deleter.dart
+++ apps/dev_feed/lib/bookmark/model/bookmark_deleter.dart
@@ -1,10 +1,6 @@
 import 'package:dev_feed/bookmark/cache/bookmark_store.dart';
-import 'package:dev_feed/bookmark/model/bookmark.dart';
-import 'package:dev_feed/bookmark/model/bookmark_creator.dart';
-import 'package:dev_feed/posts_feed/model/model.dart';
 
 abstract class BookmarkDeleter {
-  Future<void> deleteFor(Post post);
   Future<void> deleteById(String id);
 }
 
@@ -12,12 +8,6 @@ class BookmarkDeleterImpl implements BookmarkDeleter {
   final BookmarkStore _store;
 
   BookmarkDeleterImpl(this._store);
-
-  @override
-  Future<void> deleteFor(Post post) async {
-    final bookmark = Bookmark(id: uuid.v4(), post: post);
-    await _store.insert(bookmark);
-  }
 
   @override
   Future<void> deleteById(String id) async {
